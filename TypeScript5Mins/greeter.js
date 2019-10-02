3e994c301bfb81e6c192c9230d01061c1531d43e
Eunsang Jeon <eunsang.jeon.127@gmail.com>
2019-10-02 04:39:21 +0000

    changed structure

---
+++ TypeScript5Mins/greeter.js
@@ -0,0 +1,14 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInital + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Eunsang", "N.", "Jeon");
+document.body.textContent = greeter(user);
