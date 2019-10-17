581a4f854ff364964e0bf9e25ed714a925a556a8
Eunsang Jeon <eunsang.jeon.127@gmail.com>
2019-10-17 05:06:11 +0000

    working on abstract classes

---
+++ Handbook/Classes.js
@@ -0,0 +1,210 @@
+//// Classes
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Greeter = /** @class */ (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
+var greeting1 = new Greeter("world");
+console.log(greeting1.greet());
+//// Inheritance
+var Animal = /** @class */ (function () {
+    function Animal() {
+    }
+    Animal.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 0; }
+        console.log("Animal moved " + distanceInMeters + "m.");
+    };
+    return Animal;
+}());
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Dog.prototype.bark = function () {
+        console.log('Woof! Woof!');
+    };
+    return Dog;
+}(Animal));
+var dog1 = new Dog();
+dog1.bark();
+dog1.move(10);
+var AnimalComplex = /** @class */ (function () {
+    function AnimalComplex(theName) {
+        this.name = theName;
+    }
+    AnimalComplex.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 0; }
+        console.log(this.name + " moved " + distanceInMeters + "m.");
+    };
+    return AnimalComplex;
+}());
+var Snake = /** @class */ (function (_super) {
+    __extends(Snake, _super);
+    function Snake(name) {
+        return _super.call(this, name) || this;
+    }
+    Snake.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 5; }
+        console.log("Slithering...");
+        _super.prototype.move.call(this, distanceInMeters);
+    };
+    return Snake;
+}(AnimalComplex));
+var Horse = /** @class */ (function (_super) {
+    __extends(Horse, _super);
+    function Horse(name) {
+        return _super.call(this, name) || this;
+    }
+    Horse.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 45; }
+        console.log("Galloping...");
+        _super.prototype.move.call(this, distanceInMeters);
+    };
+    return Horse;
+}(AnimalComplex));
+var sam = new Snake("Sammy the Python");
+var tom = new Horse("Tommy the Palomino");
+sam.move();
+tom.move(33);
+//// Private and Protected
+var Animal2 = /** @class */ (function () {
+    function Animal2(theName) {
+        this.name = theName;
+    }
+    return Animal2;
+}());
+var cat1 = new Animal2("Cat");
+// cat1.name = "CAT"; // of course it occurs error
+var Animal3 = /** @class */ (function () {
+    function Animal3(theName) {
+        this.name = theName;
+    }
+    return Animal3;
+}());
+var Rhino = /** @class */ (function (_super) {
+    __extends(Rhino, _super);
+    function Rhino() {
+        return _super.call(this, "Rhino") || this;
+    }
+    return Rhino;
+}(Animal3));
+var Employee = /** @class */ (function () {
+    function Employee(theName) {
+        this.name = theName;
+    }
+    return Employee;
+}());
+var animal3 = new Animal3("Goat");
+var rhino = new Rhino();
+var employee = new Employee("Bob");
+animal3 = rhino;
+// animal3 = employee; // Error: 'Animal' and 'Employee' are not compatible
+var Person1 = /** @class */ (function () {
+    function Person1(name) {
+        this.name = name;
+    }
+    return Person1;
+}());
+var Employee1 = /** @class */ (function (_super) {
+    __extends(Employee1, _super);
+    function Employee1(name, department) {
+        var _this = _super.call(this, name) || this;
+        _this.department = department;
+        return _this;
+    }
+    Employee1.prototype.getElevatorPitch = function () {
+        return "Hello, my name is " + this.name + " and I work in " + this.department;
+    };
+    return Employee1;
+}(Person1));
+var howard1 = new Employee1("Howard", "Sales");
+console.log(howard1.getElevatorPitch());
+// console.log(howard1.name); //error 
+var Person2 = /** @class */ (function () {
+    function Person2(theName) {
+        this.name = theName;
+    }
+    return Person2;
+}());
+var Employee2 = /** @class */ (function (_super) {
+    __extends(Employee2, _super);
+    function Employee2(name, department) {
+        var _this = _super.call(this, name) || this;
+        _this.department = department;
+        return _this;
+    }
+    Employee2.prototype.getElevatorPitch = function () {
+        return "Hello, my name is " + this.name + " and I work in " + this.department + ".";
+    };
+    return Employee2;
+}(Person2));
+var howard2 = new Employee2("Howard", "Sales");
+// let john2 = new Person2("John"); // Error: The 'Person' constructor is protected
+//// Readonly modifier
+var Octopus = /** @class */ (function () {
+    function Octopus(theName) {
+        this.numberOfLegs = 8;
+        this.name = theName;
+    }
+    return Octopus;
+}());
+var dad = new Octopus("Man with the 8 strong legs");
+// dad.name = "Man with the 3-piece suit"; // error! name is readonly.
+//// Getter and Setter
+//const fullNameMaxLength = 10;
+//
+//class Employee3 {
+//	private  _fullName: string;
+//
+//	get fullName(): string {
+//		return this._fullName;
+//	}
+//
+//	set fullName(newName: string) {	
+//		if (newName && newName.length > fullNameMaxLength) {
+//			throw new Error("excess of max length for a name")
+//		}
+//		this._fullName = newName;
+//	}
+//}
+//
+//let employee3 = new Employee3();
+//employee3.fullName = "Bob Odenkirk";
+//if (employee3.fullName) {
+//	console.log(employee3.fullName);
+//}
+//// Static Properties
+var Grid = /** @class */ (function () {
+    function Grid(scale) {
+        this.scale = scale;
+    }
+    Grid.prototype.calculateDistanceFromOrigin = function (point) {
+        var xDist = (point.x - Grid.origin.x);
+        var yDist = (point.y - Grid.origin.y);
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+    };
+    Grid.origin = { x: 0, y: 0 };
+    return Grid;
+}());
+var grid1 = new Grid(1.0); // 1x scale
+var grid2 = new Grid(5.0); // 5x scale
+console.log(grid1.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+console.log(grid2.calculateDistanceFromOrigin({ x: 10, y: 10 }));
--- Handbook/Classes.ts
+++ Handbook/Classes.ts
@@ -1,8 +1,214 @@
-//Classes
+//// Classes
 
 class Greeter {
 	greeting: string;
 	constructor(message: string) {
-
+		this.greeting = message;
+	}
+	greet() {
+		return "Hello, " + this.greeting;
 	}
 }
+
+let greeting1 = new Greeter("world");
+console.log(greeting1.greet());
+
+//// Inheritance
+
+class Animal {
+	move(distanceInMeters: number = 0) {
+		console.log(`Animal moved ${distanceInMeters}m.`);
+	}
+}
+
+class Dog extends Animal {
+	bark() {
+		console.log('Woof! Woof!');
+	}
+}
+
+const dog1 = new Dog();
+dog1.bark();
+dog1.move(10);
+
+class AnimalComplex {
+	name: string;
+	constructor(theName: string) {
+		this.name = theName;
+	}
+	move(distanceInMeters: number = 0){
+		console.log(`${this.name} moved ${distanceInMeters}m.`);
+	}
+}
+
+class Snake extends AnimalComplex {
+	constructor(name: string) {
+		super(name);
+	}
+	move(distanceInMeters = 5) {
+		console.log("Slithering...");
+		super.move(distanceInMeters);
+	}
+}
+
+class Horse extends AnimalComplex {
+	constructor(name: string) {
+		super(name);
+	}
+	move(distanceInMeters = 45) {
+		console.log("Galloping...");
+		super.move(distanceInMeters);
+	}
+}
+
+let sam = new Snake("Sammy the Python");
+let tom: AnimalComplex = new Horse("Tommy the Palomino");
+
+sam.move();
+tom.move(33);
+
+//// Private and Protected
+
+class Animal2 {
+	private name: string;
+	constructor(theName: string) {
+		this.name = theName;
+	}
+}
+
+let cat1 = new Animal2("Cat");
+// cat1.name = "CAT"; // of course it occurs error
+
+class Animal3 {
+	    private name: string;
+	    constructor(theName: string) { this.name = theName; }
+}
+
+class Rhino extends Animal3 {
+	    constructor() { super("Rhino"); }
+}
+
+class Employee {
+	    private name: string;
+	    constructor(theName: string) { this.name = theName; }
+}
+
+let animal3 = new Animal3("Goat");
+let rhino = new Rhino();
+let employee = new Employee("Bob");
+
+animal3 = rhino;
+// animal3 = employee; // Error: 'Animal' and 'Employee' are not compatible
+
+class Person1{
+	protected name: string;
+	constructor(name: string) {
+		this.name = name;
+	}
+}
+
+class Employee1 extends Person1 {
+	private department: string;
+
+	constructor(name: string, department: string) {
+		super(name);
+		this.department = department;
+	}
+
+	public getElevatorPitch() {
+		return `Hello, my name is ${this.name} and I work in ${this.department}`;
+	}
+}
+
+let howard1 = new Employee1("Howard", "Sales");
+console.log(howard1.getElevatorPitch());
+// console.log(howard1.name); //error 
+
+class Person2 {
+	    protected name: string;
+	    protected constructor(theName: string) { this.name = theName; }
+}
+
+class Employee2 extends Person2 {
+	private department: string;
+
+	constructor(name: string, department: string) {
+		            super(name);
+		            this.department = department;
+	}
+
+	public getElevatorPitch() {
+		return `Hello, my name is ${this.name} and I work in ${this.department}.`;
+	}
+}
+
+let howard2 = new Employee2("Howard", "Sales");
+// let john2 = new Person2("John"); // Error: The 'Person' constructor is protected
+
+//// Readonly modifier
+
+class Octopus {
+	    readonly name: string;
+	    readonly numberOfLegs: number = 8;
+	    constructor (theName: string) {
+		            this.name = theName;
+		        }
+}
+let dad = new Octopus("Man with the 8 strong legs");
+// dad.name = "Man with the 3-piece suit"; // error! name is readonly.
+
+//// Getter and Setter
+
+//const fullNameMaxLength = 10;
+//
+//class Employee3 {
+//	private  _fullName: string;
+//
+//	get fullName(): string {
+//		return this._fullName;
+//	}
+//
+//	set fullName(newName: string) {	
+//		if (newName && newName.length > fullNameMaxLength) {
+//			throw new Error("excess of max length for a name")
+//		}
+//		this._fullName = newName;
+//	}
+//}
+//
+//let employee3 = new Employee3();
+//employee3.fullName = "Bob Odenkirk";
+//if (employee3.fullName) {
+//	console.log(employee3.fullName);
+//}
+
+//// Static Properties
+
+class Grid {
+	static origin = {x: 0, y: 0};
+	calculateDistanceFromOrigin(point: {x: number; y: number;}) {
+		let xDist = (point.x - Grid.origin.x);
+		let yDist = (point.y - Grid.origin.y);
+		return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+	}
+	constructor (public scale: number) {}
+}
+
+let grid1 = new Grid(1.0);  // 1x scale
+let grid2 = new Grid(5.0);  // 5x scale
+
+console.log(grid1.calculateDistanceFromOrigin({x: 10, y: 10}));
+console.log(grid2.calculateDistanceFromOrigin({x: 10, y: 10}));
+
+//// Abstract Classes
+
+abstract class Department {
+	constructor(public name: string) {}
+
+	printName(): void {
+		console.log("Department name: " + this.name);
+	}
+
+
+}
+
