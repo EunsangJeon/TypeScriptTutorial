1f9dcadbb82fd3c54682a9a974002c184eaf29c9
Eunsang Jeon <eunsang.jeon.127@gmail.com>
2019-11-14 09:09:19 +0000

    working on generics

--- Handbook/Generics.js
+++ Handbook/Generics.js
@@ -1,14 +1,10 @@
 // Generic basic form
-
-function genericBasicFunction<T>(arg: T): T {
-	return arg;
+function genericBasicFunction(arg) {
+    return arg;
 }
-
 console.log(genericBasicFunction(33));
 console.log(genericBasicFunction("Basic Function"));
-
 // Generic should be "common"
-
 // function loggingIdentity<T>(arg: T): T {
 // 	    console.log(arg.length);  // Error: T doesn't have .length
 // 	    return arg;
@@ -24,46 +20,24 @@ console.log(genericBasicFunction("Basic Function"));
 // 	    return arg;
 // }
 //
-
 // Generic and interfaces
-
-function identity<T>(arg: T): T {
-	return arg;
+function identity(arg) {
+    return arg;
 }
-
-let myIdentity1: <T>(arg: T) => T = identity;
-let myIdentity2: <U>(arg: U) => U = identity;
-let myIdentity3: {<T>(arg: T): T} = identity;
-
-interface GenericIdentityFn1 {
-	<T>(arg: T): T;
-}
-
-let myIdentity4: GenericIdentityFn1 = identity;
-
-interface GenericIdentityFn2<T> {
-	(arg: T): T;
-}
-
-let myIdentity5: GenericIdentityFn2<number> = identity;
-
+var myIdentity1 = identity;
+var myIdentity2 = identity;
+var myIdentity3 = identity;
+var myIdentity4 = identity;
+var myIdentity5 = identity;
 console.log(myIdentity5(9));
-
 // In addition to generic interfaces, it is also possible to create generic classes.
 // It is not possible to create generic enums and namespaces.
-
-class GenericClass<T> {
-	zeroValue: T;
-	add: (x: T, y: T) => T;
-}
-
-let myGenericNumber = new GenericClass<number>();
+var GenericClass = /** @class */ (function () {
+    function GenericClass() {
+    }
+    return GenericClass;
+}());
+var myGenericNumber = new GenericClass();
 myGenericNumber.zeroValue = 0;
-myGenericNumber.add = function(x, y){ return x + y };
-
-console.log(myGenericNumber.add(3,4));
-
-// Well actually, you can instance a class with string as generic 'cause it doesn't conflict to any.
-
- 
-
+myGenericNumber.add = function (x, y) { return x + y; };
+console.log(myGenericNumber.add(3, 4));
